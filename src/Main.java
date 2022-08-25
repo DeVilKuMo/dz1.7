8a15d6418f80689682aa2a13d10c2a77fae357d5
71 <devilkumo@mail.ru>
2022-08-25 18:41:47 +0300

    дз сделал

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,36 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();task2();task3();
+    }public static void task1() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(" ФИО сотрудника - " + fullName);
+        System.out.println();
+    }public static void task2() {
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+
+        String fullName = lastName + " " + firstName + " " + middleName;
+        String result = fullName.toUpperCase();
+        System.out.println("Данные ФИО сотрудника для заполнения отчета — " + result);
+        System.out.println();
+    }public static void task3(){
+        String fullName = "Иванов Семён Семёнович";
+        String[] split = fullName.split(" ");
+        String result = " ";
+        for (String current : split){
+            if (current.contains("ё")) {
+                String tex = current.replace("ё", "е");
+                result += tex;
+            }else {
+                result += current;
+            }
+            result += " ";
+        }
+        System.out.println("Данные ФИО сотрудника —" + result);
     }
 }
